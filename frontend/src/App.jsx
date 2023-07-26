aa6b6e372c9eacdc3c2ae2d857b307ebd2583a26
AdnanRohmatKurniansah <atnandimas@gmail.com>
2023-07-26 20:24:59 +0700

    init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Box from './components/Box'
+
+function App() {
+  return (
+    <div className='App'style={{ margin: '100px' }}>
+      <Box value="X" onClick={null}/>
+      <Box value="X" onClick={null}/>
+      <Box value="X" onClick={null}/>
+      <Box value="X" onClick={null}/>
+      <Box value="X" onClick={null}/>
+      <Box value="X" onClick={null}/>
+      <Box value="X" onClick={null}/>
+      <Box value="X" onClick={null}/>
+    </div>
+  )
+}
+
+export default App
