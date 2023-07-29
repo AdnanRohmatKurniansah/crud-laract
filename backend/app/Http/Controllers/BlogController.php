0ad9e165d403789515c080407ea397ca968470b1
AdnanRohmatKurniansah <atnandimas@gmail.com>
2023-07-29 11:35:25 +0700

    make endpoint and fix cors

--- backend/app/Http/Controllers/BlogController.php
+++ backend/app/Http/Controllers/BlogController.php
@@ -2,9 +2,99 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class BlogController extends Controller
 {
-    //
+    public function index() 
+    {
+        return Blog::select('id','title', 'excerpt', 'description','image')->get();
+    }
+
+    public function store(Request $request) 
+    {
+        $data = $request->validate([
+            'title' => 'required|max:250|min:5',
+            'description' => 'required',
+            'image'=>'required|image|max:2048'
+        ]);
+
+        try{
+            if($request->file('image')) {
+                $data['image'] = $request->file('image')->store('blog-images');
+            } 
+
+            $data['excerpt'] = Str::limit($data['description'], 150);
+
+            Blog::create($data);
+
+            return response()->json([
+                'message'=> 'Blog Created Successfully'
+            ]);
+
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return response()->json([
+                'message'=>'Something goes wrong while creating a blog'
+            ], 500);
+        }
+    }
+    public function show(Blog $blog) 
+    {
+        return response()->json([
+            'blog' => $blog
+        ]);
+    }
+    public function update(Request $request, Blog $blog) 
+    {
+        $data = $request->validate([
+            'title' => 'required|max:250|min:5',
+            'description' => 'required',
+            'image'=>'required|image|max:2048'
+        ]);
+
+        try {
+            if ($request->file('image')) {
+                if ($request->input('oldImage')) {
+                    Storage::delete($request->input('oldImage'));
+                }
+                $data['image'] = $request->file('image')->store('blog-images');
+            }  
+
+            Blog::where('id', $blog->id)
+                ->update($data);
+
+            return response()->json([
+                'message' => 'Blog Updated Successfully!!'
+            ]);    
+
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return response()->json([
+                'message' => 'Something goes wrong while updating a blog'
+            ], 500);
+        }
+    }
+    public function destroy(Blog $blog) 
+    {
+        try {
+            if ($blog->image) {
+                Storage::delete($blog->image);
+            }
+            
+            Blog::destroy($blog->id);
+            return response()->json([
+                'message' => 'Blog Removed Successfully'
+            ]);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return response()->json([
+                'message' => 'Something goes wrong while deleting a blog'
+            ]);
+        }
+    }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/blogs', [BlogController::class, 'index']);
+Route::post('/blogs', [BlogController::class, 'store']);
+Route::get('/blogs/{blog:id}', [BlogController::class, 'show']);
+Route::put('/blogs/{blog:id}', [BlogController::class, 'update']);
+Route::delete('/blogs/{blog:id}', [BlogController::class, 'destroy']);
